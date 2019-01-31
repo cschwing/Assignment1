73c07e5ca273f08b4cf256af5a9f92ff6eea960a
connorschwing <cas15c@my.fsu.edu>
2019-01-30 23:11:31 -0500

    Created class SimpleHammer.h and SimpleHammer.cppp. Copied structure of CommonSword.h and CommonSword.hpp. In SimpleHammer.cpp, made the added change to ignore all armor points if armor is less than 30

--- SimpleHammer.h
+++ SimpleHammer.h
@@ -1,1 +1,31 @@
-#pragma once
+/*
+ * File:   SimpleHammer.h
+ * Author: Connor Schwinghammer <cas15c@my.fsu.edu>
+ *
+ * Created on January 30, 2019, 10:59 PM
+ */
+
+#include <string>
+#include "Weapon.h"
+
+#ifndef SIMPLEHAMMER_H
+#define SIMPLEHAMMER_H
+
+ /**
+  * Defines the behavior of a simple hammer (hitpoints = 25, if armor < 30, hammer ignores
+  * all armor points)
+  */
+class SimpleHammer : public Weapon {
+public:
+
+	SimpleHammer() : Weapon("Simple hammer", 25.0) { //Calls Weapon(name, hitpoints) constructor with values Simple Hammer and 40.0
+	}
+
+	virtual ~SimpleHammer() {};
+
+	virtual double hit(double armor);
+
+};
+
+#endif /* SIMPLEHAMMER_H */
+
